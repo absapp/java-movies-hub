e60e6aa408b67a88bdee4ea2824c56c5cb913373
Ivan <absapp@yandex.ru>
2026-04-19 22:48:08 +0500

    реализовать функциональность финального проекта

--- src/main/ru/practicum/moviehub/http/BaseHttpHandler.java
+++ src/main/ru/practicum/moviehub/http/BaseHttpHandler.java
@@ -1,7 +1,29 @@
 package ru.practicum.moviehub.http;
 
+import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public abstract class BaseHttpHandler implements HttpHandler {
 
+    protected static final String CT_JSON =  "application/json; charset=UTF-8";
+    protected static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
+
+    protected void sendJson(HttpExchange ex, int status, String json) throws IOException {
+        byte[] responseBytes = json.getBytes(DEFAULT_CHARSET);
+        ex.getResponseHeaders().set("Content-Type", CT_JSON);
+        try (OutputStream os = ex.getResponseBody()) {
+            ex.sendResponseHeaders(status, responseBytes.length);
+            os.write(responseBytes);
+        }
+    }
+
+    protected void sendNoContent(HttpExchange ex) throws java.io.IOException {
+        ex.getResponseHeaders().set("Content-Type", CT_JSON);
+        ex.sendResponseHeaders(204, -1);
+    }
 }
--- src/main/ru/practicum/moviehub/model/Movie.java
+++ src/main/ru/practicum/moviehub/model/Movie.java
@@ -1,5 +1,78 @@
 package ru.practicum.moviehub.model;
 
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Random;
+import java.util.Set;
+
 public class Movie {
 
+    private String title;
+    private int year;
+    private int id;
+    private static Set<Integer> usedIds = new HashSet<>();
+
+    public Movie() {
+        this.id = generateUniqueId();
+    }
+
+    public Movie(String title, int year) {
+        this();
+        this.title = title;
+        this.year = year;
+    }
+
+    private int generateUniqueId() {
+        int newId;
+        do {
+            newId = new Random().nextInt(10000);  // больший диапазон
+        } while (usedIds.contains(newId));
+        usedIds.add(newId);
+        return newId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Movie movie = (Movie) o;
+        return year == movie.year && Objects.equals(title, movie.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, year);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "title='" + title + '\'' +
+                ", year=" + year +
+                ", id=" + id +
+                '}';
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
